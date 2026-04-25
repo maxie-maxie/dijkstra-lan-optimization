82a9a11e0f8a17024207c4f8da454b3bfac01a80
Wateraice727 <minhleevanw@gmail.com>
2026-04-25 20:20:52 +0700

    Initial commit

---
+++ Console/Graph/FastIO.cs
@@ -0,0 +1,32 @@
+﻿namespace FastInputOutput
+{
+    public class Scanner
+    {
+        private string[] tokens;
+        private int pointer;
+        public Scanner()
+        {
+            tokens = new string[0];
+            pointer = 0;
+        }
+        public string Next()
+        {
+            while (pointer >= tokens.Length)
+            {
+                string line = Console.ReadLine();
+                if (line == null) return null;
+                tokens = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                pointer = 0;
+            }
+            return tokens[pointer++];
+        }
+        public int NextInt32()
+        {
+            return int.Parse(Next());
+        }
+        public long NextInt64()
+        {
+            return long.Parse(Next());
+        }
+    }
+}
---
+++ Console/Graph/Program.cs
@@ -0,0 +1,35 @@
+﻿using Graph;
+using FastInputOutput;
+
+namespace Workspace
+{
+    internal class Program
+    {
+        static void Weighted()
+        {
+            Scanner sc = new Scanner();
+            int vertex = sc.NextInt32(), edge = sc.NextInt32();
+            EdgeGraph wg = new EdgeGraph(vertex);
+
+            for (int i = 0; i < edge; i++)
+            {
+                int u = sc.NextInt32(), v = sc.NextInt32();
+                long w = sc.NextInt64();
+                wg.AddEdge(u, v, w, true);
+            }
+
+            long[] distance = wg.Dijkstra(1);
+            for (int i = 1; i < distance.Length; i++) 
+            {
+                if (distance[i] < wg.Infinity) Console.Write(distance[i] + " ");
+                else Console.Write("-1 ");
+            }
+        }
+        static void Main()
+        {
+            Console.Clear();
+
+            Weighted();
+        }
+    }
+}
